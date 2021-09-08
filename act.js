5199bda5dad6a3141df04dba7e561a3ad0034b91
pradyunya <82073931+pradyunya@users.noreply.github.com>
2021-09-08 05:58:52 -0700

    php website

---
+++ act.js
@@ -0,0 +1,9 @@
+var i=document.getElementById("nam").value;
+ function valid()
+{
+if(i=="")
+{
+    alert("Enter all fields");
+    return false;
+}
+}
---
+++ x.php
@@ -0,0 +1,6 @@
+<?php
+if(isset($_POST['save']))
+{
+   echo "MY name is ". $_POST['nm'];
+}
+?>
